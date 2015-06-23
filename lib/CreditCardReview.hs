79217f3aae56cd80042bf6cd71f3dd99be1e17b6
Chris Kim <cj3kim@gmail.com>
2015-06-22 22:15:32 -0700

    Even more optimization

--- lib/CreditCardReview.hs
+++ lib/CreditCardReview.hs
@@ -1,8 +1,6 @@
 
 toDigits    :: Integer -> [Integer]
-toDigits n | n == 0 = []
-           | n  < 0 = []
-           | n  > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
+toDigits n | n  > 0    = toDigits (n `div` 10) ++ [n `mod` 10]
            | otherwise =  []
 
 
