853de4daa16439071916ee3f9a0a0b79cfb38709
Chris Kim <cj3kim@gmail.com>
2015-06-22 22:15:23 -0700

    Started over and refactored.

---
+++ lib/CreditCardReview.hs
@@ -0,0 +1,10 @@
+
+toDigits    :: Integer -> [Integer]
+toDigits n | n == 0 = []
+           | n  < 0 = []
+           | n  > 0 = toDigits (n `div` 10) ++ [n `mod` 10]
+           | otherwise =  []
+
+
+toDigitsRev :: Integer -> [Integer]
+toDigitsRev n = reverse (toDigits n)
