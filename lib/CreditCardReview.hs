79cec297affe8aa1d13f1103b876d02e70d38813
Chris Kim <cj3kim@gmail.com>
2015-06-22 22:39:01 -0700

    Refactored checksum formula

--- lib/CreditCardReview.hs
+++ lib/CreditCardReview.hs
@@ -1,8 +1,22 @@
 
-toDigits    :: Integer -> [Integer]
+toDigits :: Integer -> [Integer]
 toDigits n | n  > 0    = toDigits (n `div` 10) ++ [n `mod` 10]
            | otherwise =  []
 
 
 toDigitsRev :: Integer -> [Integer]
 toDigitsRev n = reverse (toDigits n)
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther [] = []
+doubleEveryOther  n = reverse (zipWith (*) (reverse n) (cycle([1,2])))
+
+
+computeChecksum :: Integer -> Integer
+computeChecksum n = sum (doubleEveryOther (toDigits n))
+
+validateChecksum :: Integer -> Bool
+validateChecksum n = if n `mod` 10 == 0 then True else False
+
+
+
